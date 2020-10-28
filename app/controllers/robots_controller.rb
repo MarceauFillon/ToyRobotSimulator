08d7b26e92ca84d9c969684e9b8e7aa7db0c970d
MarceauFillon <marceau-fillon@outlook.fr>
2020-10-28 20:40:45 +1100

    Control that the code is easy to read and commented
    Added comments.
    Changed tabulation and linespace for better readability.

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,123 +1,154 @@
 class RobotsController < ApplicationController
+    # Action returning the application page
+    # Returns the first robot in the list if it exists
+    # The maximum number of robot is one.
     def index
         if Robot.all().any?
-            @robot = Robot.first
+            @robot = Robot.first # Return the first and only robot
         end
     end
 
+    # Action useful to debug the application
+    # Returns a robot with a specific id
+    # The show view displays the robot attributes
     def show
         @robot = Robot.find(params[:id])
     end
 
+    # Action to create a robot
+    # No input is taken from the user and the robot is initialised at 0,0 and is not on the table
+    # If successful, a script is called to enable the place commands and convert the creation button into a destruction button.
+    # If the user interface failed and the creation button is not disabled, the user can still not create a robot.
     def create
         if !Robot.all().any?
             @robot = Robot.new({x:0, y:0, orientation:0, on_table:false})
             @robot.save
             respond_to do |format|
-                format.js {render "make"}
+                format.js {render "make"} # Call the 'make' script updating the creation/deletion button.
             end
-        else
+        else # If the database already contains a robot, alert the user that no more robot can be created.
             render js: "alert('The robot is already created!')"
         end
     end
 
+    # Action to place the robot on the table
+    # This action can only be perform with the attributes x, y and orientation in the POST request
+    # If successful, it calls the 'place' script, updating the robot position in the UI and enabling the movement buttons.
     def place
-        @robot = Robot.find(params[:id])
+        @robot = Robot.find(params[:id]) # Find the robot with the specified id
 
-        if !@robot.on_table
+        if !@robot.on_table # Place the robot on the table
             @robot.on_table = true
         end
 
-        @old_position = [@robot.x, @robot.y]
+        @old_position = [@robot.x, @robot.y] # Save the old position to delete the robot from its old position in the UI
         
         respond_to do |format|
-            if @robot.update(robot_parameters)
-                format.js
+            if @robot.update(robot_parameters) # Check that the parameters x, y and orientation were given (and only those)
+                format.js # Call the place script to update the UI
             end
         end 
     end
 
+    # Action to move the robot on the table
+    # The robot can move one unit in the direction it's oriented but cannot go out of the table.
+    # If the movement is invalid (i.e going out of the table), the 'error' script is called to warn the user.
+    # If successful, it calls the 'place' script, updating the robot position in the UI.
     def move
-        @robot = Robot.find(params[:id])
-        @old_position = [@robot.x, @robot.y]
+        @robot = Robot.find(params[:id]) # Find the robot with the specified id
+        @old_position = [@robot.x, @robot.y] # Save old position to update the table in the UI
 
-        if !@robot.move
+        if !@robot.move # If the move is invalid, this condition is false and the error script is called
             return_error
             return
         end
         
         @robot.save()
-        return_place
+        return_place # Calls the 'place' script
     end
 
+    # Action to rotate the robot on the table
+    # The robot can rotate 90 degrees at a time either on its right or on its left.
+    # If the direction is not left or right, the error script is called to warn the user.
+    # If successful, it calls the 'place' script, updating the robot orientation in the UI.
     def rotate
-        @robot = Robot.find(params[:id])
-        @old_position = [@robot.x, @robot.y]
+        @robot = Robot.find(params[:id]) # Find the robot with the specified id
+        @old_position = [@robot.x, @robot.y] # Save old position to update the table in the UI
 
-        direction = params[:direction]
+        direction = params[:direction] # Get the direction parameter from the POST request
 
-        if !@robot.rotate(direction)
+        if !@robot.rotate(direction) # If the direction was not valid, call the error script
             return_error
             return
         end
         
         @robot.save()
-        return_place
+        return_place # Call the place script to update the UI
 
     end
 
+    # Action to report the robot state in the scrolling log
+    # The log will output the position of the robot on the table and its orientation
+    # Error messages will be output if the robot does not exist or is not on the table
+    # For each request, this action will call the 'report' script to update the scrolling log. 
     def report
-        @report_result = ""
+        @report_result = "" # Initiate output
         id = params[:id]
-        if !id
+        if !id # If no id is passed, no robot has been created.
             @report_result = "No robot"
-            return_report
+            return_report # Call 'report' script to update the UI
             return 
         end
 
-        robot = Robot.find(id)
+        robot = Robot.find(id) # Get the robot with the specified ID
 
-        if !robot.on_table
+        if !robot.on_table # Robot not placed on the table
             @report_result = "Robot not on table"
-            return_report
+            return_report # Call 'report' script to update the UI
             return
         end
 
-        @report_result = "%d %d %s" % [robot.x, robot.y, robot.get_orientation_string]
+        @report_result = "%d %d %s" % [robot.x, robot.y, robot.get_orientation_string] # Format the output with the position and orientation
 
-        return_report
+        return_report # Call 'report' script to update the UI
         
     end
 
+    # Action to destroy a robot
+    # If successful, a script is called to disable the place and movement commands and convert the destruction button into a creation button.
     def destroy
         @robot = Robot.find(params[:id])
-        @old_position = [@robot.x, @robot.y]
+        @old_position = [@robot.x, @robot.y] # Save the old position to delete robot from UI
 
         @robot.destroy
-        @robot = nil
+        @robot = nil # Setting the robot to let the views know that this robot is not in the table anymore
 
         respond_to do |format|
-            format.js {render "make"}
+            format.js {render "make"} # Update the robot commands in the UI
         end
     end
 
     private
+        # Private function to control that x, y and orientation are passed in a request (and only those)
         def robot_parameters
             params.require(:robot).permit(:x, :y, :orientation)
         end
 
+        # Calls the 'error' script
         def return_error
             respond_to do |format|
                 format.js {render "error"}
             end
         end
 
+        # Calls the 'report' script
         def return_report
             respond_to do |format|
                 format.js {render "report"}
             end 
         end
 
+        # Calls the 'place' script
         def return_place
             respond_to do |format|
                 format.js {render "place"}
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,16 +2,23 @@ class Robot < ApplicationRecord
     before_update :check_presence_on_table
     before_save :check_uniqueness
 
-    validates :x, inclusion:{ :in => 0..4}
+    # Validation on coordinates to prevent coordinates outside of the table
+    # Validation on the orientation to only authorize 4 angles (East being 0 and North being 270)
+    validates :x, inclusion:{ :in => 0..4} 
     validates :y, inclusion:{ :in => 0..4}
     validates :orientation, inclusion:{ :in => (0..270).step(90)}
     validates :on_table, inclusion:{ :in => [true, false]}
 
+    # Function to move the robot one unit in the direction of its orientation
     def move
+        # Robot cannot move if not placed on the table.
         if !self.on_table
             return false
         end
 
+        # Assigning new coordinates depending on the orientation
+        # The robot can only move 1 unit at a time
+        # If the new coordinate is outside of the table, return false
         case self.orientation
         when 0
             if !(self.y + 1).between?(0,4)
@@ -42,37 +49,48 @@ class Robot < ApplicationRecord
             end
 
         else
-            return false
+            return false # If the orientation is none of the authorised values, return false
         end
 
         return true
     end
 
+    # Function to rotate the robot in the specified direction, either left or right
     def rotate(direction)
-        if !self.on_table
+        if !self.on_table # No rotation is possible if the robot has not been placed
             return false
         end
 
         new_orientation = 0
 
+        # Turning right or left means a 90 degres rotation
+        # Left is a negative rotation
+        # Right is a positive rotation
         case direction
         when "left"
-            new_orientation = self.orientation - 90
+            new_orientation = self.orientation - 90  
+
+            # The rotation is looping in the interval [0 270] with a step of 90 
             if new_orientation < 0
-                new_orientation = 270
+                new_orientation = 270 
             end
+
         when "right"
             new_orientation = self.orientation + 90
+
+            # The rotation is looping in the interval [0 270] with a step of 90 
             if new_orientation > 270
                 new_orientation = 0
             end
+
         else
-            return false
+            return false # If the direction was neither right or left, return false
         end
 
         self.orientation = new_orientation
     end
 
+    # Function returning the cardinal orientation (North, South, East, West) of the robot
     def get_orientation_string()
         case self.orientation
         when 0
@@ -84,17 +102,21 @@ class Robot < ApplicationRecord
         when 270
             return "North"
         else
-            return "Invalid"
+            return "Invalid" # If the robot orientation is not included in [0 90 180 270] the orientation is invalid
         end
     end
 
     private 
+        # Callback function called before saving
+        # If the robot does not exists and there is already a robot in the database, the save process is aborted
         def check_uniqueness
             if !self.id && Robot.all().any?
                 throw(:abort)
             end   
         end
 
+        # Callback function called before updating
+        # If the robot is not placed on the table, it cannot be updated (moved or rotated), update process is aborted
         def check_presence_on_table
             if !self.on_table
                 throw(:abort)
